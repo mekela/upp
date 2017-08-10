4b13520f92420b2286d40d3e869e660724cadd9d
alex <sania_free@i.ua>
2017-08-10 07:39:44 +0300

    first

---
+++ app/js/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	//fancybox
+	$('.fancybox').fancybox({
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
+	
+	//bxslider
+	$('.top-slider__block ul').bxSlider();
+
+	$('.contacts__right ul').bxSlider();
+});
